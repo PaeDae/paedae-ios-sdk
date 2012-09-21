f1768c81e1fd6060caf3138830d7b99edec12f48
Miguel Morales <miguel@paedae.com>
2012-09-21 13:03:52 -0700

    fixed namespace issue; added version number to header;

--- PrizeTest/PrizeTest/PaeDaePrizeSDK/PaeDaePrizeSDK.h
+++ PrizeTest/PrizeTest/PaeDaePrizeSDK/PaeDaePrizeSDK.h
@@ -4,10 +4,12 @@
 //
 //  Created by PaeDae on 8/13/12.
 //  Copyright (c) 2012 PaeDae. All rights reserved.
-//
+//  Version 0.3.0
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
+
+#define PAEDAE_SDK_VERSION 0.3.0
 
 #pragma mark - init delegate
 @protocol PaeDaeInitDelegate <NSObject>
