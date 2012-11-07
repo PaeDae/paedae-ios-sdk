5eca856e3db84ab5e037dbebb05f6ab4a7e75af0
Miguel Morales <miguel@paedae.com>
2012-11-06 17:03:36 -0800

    updates to prize test for cpi branch

--- PaeDaePrizeSDK/PaeDaePrizeSDK.h
+++ PaeDaePrizeSDK/PaeDaePrizeSDK.h
@@ -4,12 +4,12 @@
 //
 //  Created by PaeDae on 8/13/12.
 //  Copyright (c) 2012 PaeDae. All rights reserved.
-//  Version 0.3.1
+//  Version 0.3.2
 
 #import <Foundation/Foundation.h>
 #import <UIKit/UIKit.h>
 
-#define PAEDAE_SDK_VERSION 0.3.1
+#define PAEDAE_SDK_VERSION @"0.3.2"
 
 #pragma mark - init delegate
 @protocol PaeDaeInitDelegate <NSObject>
